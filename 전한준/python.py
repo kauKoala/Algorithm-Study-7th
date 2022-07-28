c31de9f5729ac17906f88adf33e67e2bf30ce993
Han joon Jeon <hjun@naver.com>
2022-07-28 10:54:37 +0900

    테스트

--- 전한준/python.py
+++ 전한준/python.py
@@ -1,14 +1,35 @@
-x,y,w,h=map(int,input().split())
+a,b,c,d=(map(int,input().split()))
 
 
 
-list=[]
-list.append(x)
-list.append(y)
-list.append(h-y)
-list.append(w-x)
-
-print(min(list))
 
 
+for i in range(4):
+    a,b,c,d=(map(int,input().split()))
+    cnt=0
+
+    list=[]
+    list.append(a)
+    list.append(b)
+    list.append(c)
+    list.append(d)
+
+    if 1 ==list[i]:
+        cnt+=1
+if cnt==0:
+    print("D")
+    
+if cnt==1:
+    print("C")
+   
+if cnt==2:
+    print("B")
+   
+if cnt==3:
+    print("A")
+   
+if cnt==4:
+    print("E")
+ 
+        
 
