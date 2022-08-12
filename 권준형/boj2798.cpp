0836750fa7954c40df8ac4d66e648cbfd750bbf8
KwonPodo <ludin9901@gmail.com>
2022-08-12 14:43:30 +0900

    boj2798 블랙잭 통과풀이

--- 권준형/boj2798.cpp
+++ 권준형/boj2798.cpp
@@ -10,19 +10,35 @@ int main()
     vector<int> v(n, 0), tmp(n, 0);
     for (int i = 0; i < n; i++)
     {
-        if (i < 3) tmp[i] = 1;
+        // if (i < 3) tmp[i] = 1;
         cin >> v[i];
     }
-    int max = 0;
-    do 
+    // int max = 0;
+    // do 
+    // {
+    //     int sum = 0;
+    //     for (int i = 0; i < n; i++)
+    //         if (tmp[i]) 
+    //             sum += v[i];
+    //     if (sum <= m)
+    //         max = std::max(max, sum);
+    // }
+    // while (next_permutation(v.begin(), v.end()));
+    // cout << max;
+
+    int sum, ans;
+    sum = ans = 0;
+    for (int i = 0; i < n-2; i++)
     {
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-            if (tmp[i]) 
-                sum += v[i];
-        if (sum <= m)
-            max = std::max(max, sum);
+        for (int j = i+1; j < n-1; j++)
+        {
+            for (int k = j+1; k < n; k++)
+            {
+                sum = v[i] + v[j] + v[k];
+                if (m >= sum)
+                    ans = std::max(ans, sum);
+            }
+        }
     }
-    while (next_permutation(v.begin(), v.end()));
-    cout << max;
+    cout << ans;
 }
